5fc15ad71e1035603c15b2c5a3492f170dc62e29
secf4ult <secf4ult@gmail.com>
2018-04-05 17:41:37 +0800

    run it background

--- streamlink/stream.sh
+++ streamlink/stream.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# steam url
 quin="https://www.douyu.com/quin"
+
 # "--player-continuous-http" used for potplayer stream
-streamlink --player "/mnt/c/Program\ Files/DAUM/PotPlayer/PotPlayerMini64.exe" --player-continuous-http $quin best
+streamlink --player "/mnt/c/Program\ Files/DAUM/PotPlayer/PotPlayerMini64.exe" --player-continuous-http $quin best &
